d390a5d29d67e687bcf328d13b633e4f37ba6b62
Cristian Coral <ccoral@tvnovedadescolombia.com>
2021-03-15 10:56:14 -0500

    Se crea crearfactura, crearproveedor y se crea home

--- php/ingresar.php
+++ php/ingresar.php
@@ -1,12 +1,12 @@
 <?php
-    $cons_usuario="root";
-    $cons_contra="";
-    $cons_base_datos="bd_gestiongastos";
+    $cons_usuario="cristian.coral";
+    $cons_contra="QWpoA1029.";
+    $cons_base_datos="GESTOR_FACTURAS";
     $cons_equipo="localhost";
 
     $obj_conexion = mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
 
-    /*
+    
     if(!$obj_conexion)
     {
         echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
@@ -15,19 +15,19 @@
     {
         echo "<h3>Conexion Exitosa PHP - MySQL</h3><hr><br>";
     }
-    */
+    
     
         $correo=$_POST['correo'];
         $clave=$_POST['clave'];
-        header("ranking.php?correo_ensesion=" . $_POST['correo']);
-        header ('snake.html' .$correo.'ingresar.php');
+        header("index.php?correo_ensesion=" . $_POST['correo']);
+        header ('index.html' .$correo.'ingresar.php');
 
         $var_consulta= "select * from usuarios where correo='$correo' and clave='$clave'";
         $var_resultado = $obj_conexion->query($var_consulta);
 
             if($var_resultado->num_rows>0)
             {
-                header("Location:juego.html");
+                header("Location:index.html");
             } else {
     
                 echo '<script language="javascript">alert("Error en la autentificacion. Correo o clave errado/a");window.location.href="login.html"</script>';
